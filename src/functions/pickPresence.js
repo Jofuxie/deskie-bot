55808c9f6e95bcd5f3a5ffe791e6eb886cd8d4ee
Jofuxie <your-geoffrey.sese@outlook.com>
2026-03-26 11:44:18 +0800

    commit

--- src/functions/pickPresence.js
+++ src/functions/pickPresence.js
@@ -1,21 +1,26 @@
 // src/functions/pickPresence.js
 const { ActivityType } = require('discord.js');
 
-// An array of presence status options (customize as needed)
 const statuses = [
   { text: 'Pomodoro sessions', type: ActivityType.Watching },
   { text: 'for /help', type: ActivityType.Listening },
   { text: 'productivity', type: ActivityType.Playing },
-  // add more statuses as desired
 ];
-const updateInterval = 300;  // in seconds, e.g. 300s = 5 minutes
+
+const updateInterval = 300; // seconds
+const startupDelay = 10; // seconds
 
 module.exports = (client) => {
   let index = 0;
-  // Rotate through the statuses array every [updateInterval] seconds
-  setInterval(() => {
+
+  const applyPresence = () => {
     const status = statuses[index];
     client.user.setActivity(status.text, { type: status.type });
     index = (index + 1) % statuses.length;
-  }, updateInterval * 1000);
+  };
+
+  setTimeout(() => {
+    applyPresence();
+    setInterval(applyPresence, updateInterval * 1000);
+  }, startupDelay * 1000);
 };
